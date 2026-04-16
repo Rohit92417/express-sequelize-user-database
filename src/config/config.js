c8aab4f37b5a9ffbc87d44df4a6e054e8847f092
Rohit Khatri <65548007+Rohit92417@users.noreply.github.com>
2026-04-16 12:55:00 +0530

    Change DATABASE_PASSWORD to hardcoded valuetruffleh
    Updated DATABASE_PASSWORD to a hardcoded value.

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,8 @@
 module.exports = {
   DATABASE_HOST: process.env.DB_HOST,
   DATABASE_USER: process.env.DB_USERNAME,
-  DATABASE_PASSWORD: process.env.DB_PASS,
+  DATABASE_PASSWORD: 'localhijsdsodj,
   DATABASE_NAME: process.env.DB_NAME,
   dialect: "mysql",
 };
 
-
-  /*
-  DATABASE_USER: "Lo9kfc2ALU",
-  DATABASE_PASSWORD: "sX19pHnxto",
-  DATABASE_NAME: "Lo9kfc2ALU",*/
