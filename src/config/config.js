b7fe69e150bcae6325723d0a78824659c13e550a
Rohit Khatri <65548007+Rohit92417@users.noreply.github.com>
2026-04-16 13:08:59 +0530

    d

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   DATABASE_HOST: process.env.DB_HOST,
   DATABASE_USER: process.env.DB_USERNAME,
-  DATABASE_PASSWORD: 'x7$kP9!qL2#vZ@8sDfR1',
+  DATABASE_PASSWORD: 'x7$kP9!qL2#vZ@8sDfR1TgH7!LpQ2zX9@vB',
   DATABASE_NAME: process.env.DB_NAME,
   dialect: "mysql",
 };
